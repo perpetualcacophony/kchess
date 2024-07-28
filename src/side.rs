1a0b59e6aa4fe977da1df93a7d1db469ed53616e
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-28 13:11:09 -0500

    added some optional rand features

--- src/side.rs
+++ src/side.rs
@@ -3,3 +3,22 @@ pub enum ChessSide {
     White,
     Black,
 }
+
+impl ChessSide {
+    pub const ARRAY: [Self; 2] = [Self::White, Self::Black];
+
+    #[cfg(feature = "rand")]
+    pub fn choose_with<R: rand::Rng>(rng: &mut R) -> Self {
+        use rand::seq::IteratorRandom;
+
+        Self::ARRAY
+            .into_iter()
+            .choose(rng)
+            .expect("array should not be empty")
+    }
+
+    #[cfg(feature = "rand_full")]
+    pub fn choose() -> Self {
+        Self::choose_with(&mut rand::thread_rng())
+    }
+}
